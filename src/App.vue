8d19690c34aae1b6d9a5c315b925f07ef332cff6
mAteeb10 <102751908+mAteeb10@users.noreply.github.com>
2024-06-07 15:24:08 +0500

    Started working on the second part

--- src/App.vue
+++ src/App.vue
@@ -30,35 +30,49 @@ export default {
 </script>
 
 <template>
-  <section class="head-1">
+  <section class="head">
     <div class="section-one">
       <h1>Part 1</h1>
       <button @click="count++">Count is: {{ count }}</button>
+    </div>
+    <div class="section-two">
+      <h1>Part 2</h1>
     </div>
   </section>
 </template>
 
 <style scoped>
-button {
+.head {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 500px;
+}
+
+.head .section-one {
+  display: flex;
+  flex-direction: column;
+  /* justify-content: flex-start; */
+  /* top: 50%; */
+  border: solid 1px;
+  /* margin-bottom: 590px; */
+  width: 1000px;
+  align-items: center;
+}
+
+.head .section-one button {
   font-weight: bold;
   display: flex;
   width: 100px;
   margin-top: 20px;
 }
-
-.section-one {
+.head .section-two {
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
+  margin-top: 20px;
   /* top: 50%; */
   border: solid 1px;
-  margin-bottom: 590px;
   width: 1000px;
-  align-items: center;
-}
-
-.head-1 {
-  display: flex;
   align-items: center;
 }
 </style>
